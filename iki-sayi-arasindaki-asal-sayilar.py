a716611860043ef81b7ac188a237e33477db151d
Hakan Akinci <hakan062.2001@gmail.com>
2024-08-30 00:45:57 +0300

    İki sayı arasındaki asal sayıları listeleyen bir program

--- iki-sayi-arasindaki-asal-sayilar.py
+++ iki-sayi-arasindaki-asal-sayilar.py
@@ -2,3 +2,19 @@
 İki sayı arasındaki asal sayıları listeleyen bir program yazın 
 (Write a program that lists the prime numbers between two given numbers).
 """
+
+def prime_numbers(start, end):
+    prime_nums = []
+    for num in range(start, end+1):
+        if num > 1:
+            for i in range(2, num):
+                if (num % i) == 0:
+                    break
+            else:
+                prime_nums.append(num)
+    return prime_nums
+
+start_num = 3
+end_num = 99
+prime_nums_res = prime_numbers(start_num, end_num)
+print(f"Prime numbers between {start_num} and {end_num} are: {prime_nums_res}")
