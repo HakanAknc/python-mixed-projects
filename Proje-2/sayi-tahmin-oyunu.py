be74fcd389ba686ed129b071a9cef92685ef7533
Hakan Akinci <hakan062.2001@gmail.com>
2024-08-30 09:32:17 +0300

    belirli bir aralıkta rastgele oluşturulan bir sayıyı tahmin etmesi gereken bir oyun

---
+++ Proje-2/sayi-tahmin-oyunu.py
@@ -0,0 +1,4 @@
+"""
+Bu proje, kullanıcının belirli bir aralıkta rastgele oluşturulan bir sayıyı tahmin etmesi gereken bir oyun oluşturmayı içerir. Program, kullanıcının tahminlerini daraltmalarına yardımcı olacak geri bildirimler sağlar. 
+"""
+print("Hello!")
